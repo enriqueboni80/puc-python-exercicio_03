4632a4798e1f52be4a6e2a8df8e7b3d06e6d38a7
rickboni80 <enriqueboni80@gmail.com>
2020-09-08 17:02:00 -0300

    first commit

--- aplicacao/models/departamento.py
+++ aplicacao/models/departamento.py
@@ -3,10 +3,20 @@ from django.db import models
 
 class DepartamentoManager(models.Manager):
 
-
+	''' Retrieving a single object (departament) '''
 	def obter_departamento_por_id(self, idDepartamento):
 		result = Departamento.objects.get(pk=idDepartamento)
 		return result
+
+	''' Retrieving all departaments '''
+	def obter_departamentos(self):
+    		result = Departamento.all()
+		return results
+
+	''' Retrieving all departaments que contem a sigla '''
+	def obter_departamentos_pela_sigla(self, sigla):
+    		result = objects.get(headline__contains=sigla)
+		return results
 
 
 class Departamento(models.Model):
