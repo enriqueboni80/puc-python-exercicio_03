3bc3e16cb169c7ec943ad1694dc07a7ea754de15
rickboni80 <enriqueboni80@gmail.com>
2020-09-10 17:45:19 -0300

    criando edit, update e delete de pessoas! (CRUD Pessoas)

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -2,39 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	
-	path('',
-		views.index, name='index'),
- 
- 	path('pessoa',
-		views.index, name='pessoa.index'),
- 
- 	path('pessoa/create/',
-		views.create, name='pessoa.create'),
-  
-    path('pessoa/<int:idPessoa>/',
-		views.show, name='pessoa.show'),
-    
-    path('pessoa/<int:idPessoa>/delete',
-		views.destroy, name='pessoa.delete'),
-
+    path("", views.index, name="index"),
+    path("pessoa", views.index, name="pessoa.index"),
+    path("pessoa/create/", views.create, name="pessoa.create"),
+    path("pessoa/store/", views.store, name="pessoa.store"),
+    path("pessoa/<int:idPessoa>/", views.show, name="pessoa.show"),
+    path("pessoa/<int:idPessoa>/edit", views.edit, name="pessoa.edit"),
+    path("pessoa/update", views.update, name="pessoa.update"),
+    path("pessoa/<int:idPessoa>/delete", views.destroy, name="pessoa.delete"),
 ]
 
-''' 
-|        | GET|HEAD  | categorias                  |                  | App\Http\Controllers\ControladorCategoria@index     | web          | -ok
-|        | GET|HEAD  | categorias/novo             |                  | App\Http\Controllers\ControladorCategoria@create    | web          | -ok
-|        | POST      | categorias           	   |                  | App\Http\Controllers\ControladorCategoria@store     | web          | -ok
-|        | GET|HEAD  | categorias/show/{id}        |                  | App\Http\Controllers\ControladorCategoria@show      | web          | -ok
-|        | GET|HEAD  | categorias/apagar/{id}      |                  | App\Http\Controllers\ControladorCategoria@destroy   | web          | -ok
-|        | GET|HEAD  | categorias/editar/{id}      |                  | App\Http\Controllers\ControladorCategoria@edit      | web          |
-|        | POST      | categorias/{id}             |                  | App\Http\Controllers\ControladorCategoria@update    | web          | 
-
-'''
-''' Verb          Path                        Action  Route Name
-GET           /users                      index   users.index
-GET           /users/create               create  users.create
-POST          /users                      store   users.store
-GET           /users/{user}               show    users.show
-GET           /users/{user}/edit          edit    users.edit
-PUT|PATCH     /users/{user}               update  users.update
-DELETE        /users/{user}               destroy users.destroy '''
+""" VERBS
+Verb          Path                        Action  Route Name
+GET           /pessoa                     index   users.index
+GET           /pessoa/create              create  users.create
+POST          /pessoa/store               store   users.store
+GET           /pessoa/{idPessoa}          show    users.show
+GET           /pessoa/{idPessoa}/edit     edit    users.edit
+PUT|PATCH     /pessoa/update              update  users.update
+DELETE        /pessoa/{idPessoa}          destroy users.destroy 
+"""
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -4,9 +4,9 @@ from .models.pessoa import Pessoa
 from django.template import loader
 
 
+""" return HttpResponse(request.POST["pessoa_id"]) """
+
 def index(request):
-    if request.method == "POST":
-        store(request)
 
     lista_p = Pessoa.objects.all()
     dados = {"listapessoas": lista_p}
@@ -14,37 +14,59 @@ def index(request):
 
 
 def create(request):
-    return render(request, "pessoa/create.html")
-
-
-""" store
-show
-edit
-update
-destroy """
+    dados = {
+        "form_action" : "pessoa.store"
+    }
+    return render(request, "pessoa/create.html", dados)
 
 def store(request):
-    data = Pessoa(
-        nome = request.POST['nome'],
-        sobrenome = request.POST['sobrenome'],
-        idade = request.POST['idade'],
-        depto_atual_id = request.POST['depto_atual'],
-        depto_chefia_id = None,
-        escolaridade = request.POST['escolaridade'],
-    )
-    data.save()
-    return HttpResponse("salvo com sucesso")
+    if request.method == "POST":
+        pessoa = Pessoa(
+            nome=request.POST["nome"],
+            sobrenome=request.POST["sobrenome"],
+            idade=request.POST["idade"],
+            depto_atual_id=request.POST["depto_atual"],
+            depto_chefia_id=None,
+            escolaridade=request.POST["escolaridade"],
+        )
+        pessoa.save()
+        return redirect("index")
+
 
 def show(request, idPessoa):
     p = Pessoa.objects.get(pk=idPessoa)
     dados = {"pessoa": p}
     return render(request, "pessoa/detalhar.html", dados)
 
+
+def edit(request, idPessoa):
+    p = Pessoa.objects.get(pk=idPessoa)
+    dados = {
+        "pessoa": p,
+        "form_action" : "pessoa.update"
+        }
+    return render(request, "pessoa/create.html", dados)
+
+def update(request):
+    idPessoa = request.POST["_pessoa_id"]
+    pessoa = Pessoa.objects.filter(pk=idPessoa)
+    pessoa.update(
+        nome=request.POST["nome"],
+        sobrenome=request.POST["sobrenome"],
+        idade=request.POST["idade"],
+        depto_atual_id=request.POST["depto_atual"],
+        depto_chefia_id=None,
+        escolaridade=request.POST["escolaridade"],
+    )
+    return redirect("index")
+    
+    
 def destroy(request, idPessoa):
     Pessoa.objects.get(pk=idPessoa).delete()
-    return redirect('index')
-    
-def testeJson(request, idPessoa):
+    return redirect("index")
+
+
+""" def testeJson(request, idPessoa):
     payload = {
         "lista": [
             {"nome": "matheus", "sobrenome": "souza"},
@@ -52,5 +74,5 @@ def testeJson(request, idPessoa):
         ]
     }
 
-    return JsonResponse(payload)
+    return JsonResponse(payload) """
 
